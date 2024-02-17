47a704243c2c0ad153dc0ee0c7926415db589f4e
Michelle Cheng <chengmic@oregonstate.edu>
2024-02-16 17:17:39 -0800

    first commit

---
+++ flask_app/database/db_credentials.py
@@ -0,0 +1,12 @@
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_chengmic'
+passwd = '8987'
+db = 'cs340_chengmic'
+
+
+# For OSU Flip Servers
+
+# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+# user = '<your-cs-340-db-username-here>'       # don't forget the CS_340 prefix
+# passwd = '<your-password-here>'               # should only be 4 digits if default
+# db = '<name-of-database-on-osu-server>'                                  
